c62ca4de9230abf09707a69dd589301b9e9663fa
Doublebl4de <82243962+Doublebl4de@users.noreply.github.com>
2021-04-30 16:42:24 -0700

    Update skills.py
    DELETED UNNECESARRY CHANGES. REALISED SPELLING IS BAD CHANGED STUFF THAT NEEDED TO BE CHANGED

--- skills.py
+++ skills.py
@@ -2,22 +2,18 @@
 import sys
 import vexiq
 #region config
-left_lift_motor  = vexiq.Motor(2)
-claw_motor       = vexiq.Motor(3)
-left_drive       = vexiq.Motor(6)
-right_lift_motor = vexiq.Motor(8, True) # Reverse Polarity
-right_drive      = vexiq.Motor(12, True) # Reverse Polarity
-joystick         = vexiq.Joystick()
+claw        = vexiq.Motor(1)
+left_lift   = vexiq.Motor(2, True) # Reverse Polarity
+left_drive  = vexiq.Motor(6)
+right_lift  = vexiq.Motor(8)
+right_drive = vexiq.Motor(12, True) # Reverse Polarity
+joystick    = vexiq.Joystick()
 #endregion config
-left_drive.reset_position()
-right_lift_motor.reset_position()
-lift_max_height = 932 # this controls the max lift:
-lift_min_height = -5
-claw_motor.stall_timeout = 2
+claw.reset_position()
 while True:
     #Controls Drive
-    left_lift_positon = left_lift_motor.position()
-    right_lift_position = right_lift_motor.position()
+    left_lift_positon = left_lift.position()
+    right_lift_position = right_lift.position()
     #Prints to screen
     vexiq.lcd_write("Left lift pos: "+str(left_lift_positon))
     vexiq.lcd_write("Righ lift pos: "+str(right_lift_position),2)
@@ -25,24 +21,21 @@ while True:
     left_drive.run(joystick.axisA())
     right_drive.run(joystick.axisD())
     #controls lift up
-    if joystick.bLup() and left_lift_positon < lift_max_height:
-        left_lift_motor.run(100)
-        right_lift_motor.run(100)
+    if joystick.bLup():
+        left_lift.run(100)
+        right_lift.run(100)
     #controls lift down
     elif joystick.bLdown():
-        left_lift_motor.run(-100)
-        right_lift_motor.run(-100)
+        left_lift.run(-100)
+        right_lift.run(-100)
     #controls lift 
     else:
-        left_lift_motor.hold()
-        right_lift_motor.hold()
+        left_lift.hold()
+        right_lift.hold()
     #controls claw
-    if claw_motor.stalled():
-        vexiq.lcd_write("claw_motor stalling",3)
-        claw_motor.run(0)
     if joystick.bRup():
-        claw_motor.run(100)
+        claw.run(100)
     elif joystick.bRdown():
-        claw_motor.run(-100)
+        claw.run(-100)
     else:
-        claw_motor.hold()
+        claw.hold()
