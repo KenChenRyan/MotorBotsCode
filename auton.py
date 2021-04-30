bcb5a395744f8dfd58996c78467d329dba31823f
Doublebl4de <82243962+Doublebl4de@users.noreply.github.com>
2021-04-30 16:22:41 -0700

    Update auton.py
    Changed... again

--- auton.py
+++ auton.py
@@ -42,7 +42,46 @@ def drivePower(left_drive,right_drive,fwd,distance):
 #         else:
 #             self.leftDrive.run_until(-self.left_power,distance)
 #             self.rightDrive.run_until(self.right_power,distance)
-
+# VEX IQ Python-Project
+import sys
+import vexiq
+#region config
+claw        = vexiq.Motor(1)
+left_lift   = vexiq.Motor(2, True) # Reverse Polarity
+left_drive  = vexiq.Motor(6)
+right_lift  = vexiq.Motor(8)
+right_drive = vexiq.Motor(12, True) # Reverse Polarity
+#endregion config
+claw.reset_position()
+while True:
+    #Controls Drive
+    left_lift_positon = left_lift.position()
+    right_lift_position = right_lift.position()
+    #Prints to screen
+    vexiq.lcd_write("Left lift pos: "+str(left_lift_positon))
+    vexiq.lcd_write("Righ lift pos: "+str(right_lift_position),2)
+    #Controls Tank Drive
+    claw.run(joystick.axisA())
+    right_drive.run(joystick.axisD())
+    #controls lift up
+    if joystick.bLup() and left_lift_positon < lift_max_height:
+        left_lift.run(100)
+        right_lift.run(100)
+    #controls lift down
+    elif joystick.bLdown():
+        left_lift.run(-100)
+        right_lift.run(-100)
+    #controls lift 
+    else:
+        left_lift.hold()
+        right_lift.hold()
+    #controls claw
+    if joystick.bRup():
+        claw_motor.run(100)
+    elif joystick.bRdown():
+        claw_motor.run(-100)
+    else:
+        claw_motor.hold()
 #main function
 # drivePower(left_drive,right_drive,True,100)
 # turn right
