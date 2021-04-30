818da3e7ebbb3d81d0da4f54596315fff8b27c0e
Doublebl4de <82243962+Doublebl4de@users.noreply.github.com>
2021-04-30 16:10:43 -0700

    Update skills.py
    Changed some stuff
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    y r u still here?????

--- skills.py
+++ skills.py
@@ -42,7 +42,46 @@ while True:
         claw_motor.run(0)
     if joystick.bRup():
         claw_motor.run(100)
+    elif joystick.bRdown():# VEX IQ Python-Project
+import sys
+import vexiq
+#region config
+claw         = vexiq.Motor(1)
+left_lift    = vexiq.Motor(2)
+left_drive   = vexiq.Motor(6)
+right_lift   = vexiq.Motor(8)
+right_drive_ = vexiq.Motor(12)
+#endregion config
+claw.reset_position()
+while True:
+    #Controls Drive
+    left_lift_positon = left_lift.position()
+    right_lift_position = right_lift.position()
+    #Prints to screen
+    vexiq.lcd_write("Left lift pos: "+str(left_lift_positon))
+    vexiq.lcd_write("Righ lift pos: "+str(right_lift_position),2)
+    #Controls Tank Drive
+    claw.run(joystick.axisA())
+    right_drive.run(joystick.axisD())
+    #controls lift up
+    if joystick.bLup() and left_lift_positon < lift_max_height:
+        left_lift.run(100)
+        right_lift.run(100)
+    #controls lift down
+    elif joystick.bLdown():
+        left_lift.run(-100)
+        right_lift.run(-100)
+    #controls lift 
+    else:
+        left_lift.hold()
+        right_lift.hold()
+    #controls claw
+    if joystick.bRup():
+        claw_motor.run(100)
     elif joystick.bRdown():
+        claw_motor.run(-100)
+    else:
+        claw_motor.hold()
         claw_motor.run(-100)
     else:
         claw_motor.hold()
