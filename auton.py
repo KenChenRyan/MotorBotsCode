bc70e30ce59c4f778a85447555b3363717e1d6c2
Doublebl4de <82243962+Doublebl4de@users.noreply.github.com>
2021-05-13 17:40:48 -0700

    Update auton.py

--- auton.py
+++ auton.py
@@ -14,12 +14,12 @@ dt          = drivetrain.Drivetrain(left_drive, right_drive, 200, 180)
 #endregion config
 
 #functions
-def drivePower(left_drive,right_drive,fwd,distance):
+def drivePower(claw,right_drive,fwd,distance):
     if fwd == True:
-        left_drive.run(100,distance)
+        claw.run(100,distance)
         right_drive.run(95,distance)
     else:
-        left_drive.run(-100,distance)
+        claw.run(-100,distance)
         right_drive.run(-95,distance)
     sys.sleep(0.25)
 
@@ -34,51 +34,51 @@ class LiftTrain:
 
 lift = LiftTrain(left_lift,right_lift)
 #main function
-drivePower(left_drive,right_drive,True,20)
+drivePower(left_drive,right_drive,True,120)
 # turn right
-dt.turn_until(80,-100)
+dt.turn_until(80,-56)
 #Inital Lift raising
-lift.raise_lift(10)
+lift.raise_lift(50)
 drivePower(left_drive,right_drive,True,488)
 #+1 point
 #go backwards
 drivePower(left_drive,right_drive,False,40)
 #turn left
-dt.turn_until(80,100)
+dt.turn_until(80,56)
 drivePower(left_drive,right_drive,True,30)
-dt.turn_until(80,-100)
+dt.turn_until(80,-56)
 drivePower(left_drive,right_drive,True,30)
 #Grabbing the double risers
 claw.run_until(100,10)
 lift.raise_lift(40)
-dt.turn_until(100,-50)
+dt.turn_until(100,-28)
 lift.raise_lift(-10)
 claw.run_raw_until(100,-10)
 drivePower(left_drive,right_drive,False,30)
 
 #move on to middle riser
-dt.turn_until(100,150)
+dt.turn_until(100,84)
 drivePower(left_drive,right_drive,True,300)
-dt.turn_until(100,100)
+dt.turn_until(100,56)
 drivePower(left_drive,right_drive,True,40)
-dt.turn_until(100,-100)
+dt.turn_until(100,-56)
 drivePower(left_drive,right_drive,True,40)
-dt.turn_until(100,-100)
+dt.turn_until(100,-56)
 drivePower(left_drive,right_drive,True,80)
 
 #move on to last set of purple
 drivePower(left_drive,right_drive,False,-40)
 #turn left to face the position of last set of risers
-dt.turn_until(100,100)
+dt.turn_until(100,56)
 drivePower(left_drive,right_drive,True,200)
-dt.turn_until(100,-100)
+dt.turn_until(100,-56)
 drivePower(left_drive,right_drive,True,30)
 claw.run_raw_until(100,10)
 lift.raise_lift(40)
 drivePower(left_drive,right_drive,False,30)
-dt.turn_until(100,200)
+dt.turn_until(100,112)
 drivePower(left_drive,right_drive,True,30)
-dt.turn_until(100,-100)
+dt.turn_until(100,-56)
 drivePower(left_drive,right_drive,True,30)
-dt.turn_until(100,-100)
+dt.turn_until(100,-56)
 drivePower(left_drive,right_drive,True,80)
