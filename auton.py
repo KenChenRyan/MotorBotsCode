0ff247486010622992e61fef340377fa2214fc36
Doublebl4de <82243962+Doublebl4de@users.noreply.github.com>
2021-05-23 13:30:38 -0700

    Update auton.py

--- auton.py
+++ auton.py
@@ -17,10 +17,10 @@ dt          = drivetrain.Drivetrain(left_drive, right_drive, 200, 180)
 def drivePower(left_drive,right_drive,fwd,distance):
     time = 1;
     temp_distance = distance;
-    if distance > 250:
-        while temp_distance > 250:
-            time = time + 1;
-            temp_distance = temp_distance - 250;
+    if distance > 300:
+        while temp_distance > 300:
+            time = time + 0.5;
+            temp_distance = temp_distance - 300;
     if fwd == True:
         left_drive.run(100,distance)
         right_drive.run(85,distance)
@@ -43,36 +43,30 @@ lift = LiftTrain(left_lift,right_lift)
 #move forward
 drivePower(left_drive,right_drive,True,160)
 # turn right
-dt.turn_until(80,-70)
+dt.turn_until(80,-65)
 #Inital Lift raising
 lift.raise_lift(100)
-drivePower(left_drive,right_drive,True,500)
+drivePower(left_drive,right_drive,True,510)
 #+1 point
+
+
+#go for middle risers
 #go backwards
-drivePower(left_drive,right_drive,False,300)
+drivePower(left_drive,right_drive,False,345)
 #turn left
-dt.turn_until(80,56)
-drivePower(left_drive,right_drive,True,560)
-dt.turn_until(80,-60)
-drivePower(left_drive,right_drive,True,250)
+dt.turn_until(80,60)
+drivePower(left_drive,right_drive,True,790)
+dt.turn_until(80,-52)
+drivePower(left_drive,right_drive,True,370)
 
-#Grabbing the double risers
-claw.run_until(100,10)
-lift.raise_lift(40)
-dt.turn_until(100,-28)
-lift.raise_lift(-10)
-claw.run_raw_until(100,-10)
-drivePower(left_drive,right_drive,False,30)
-
-#move on to middle riser
-dt.turn_until(100,84)
+#move on to last set of purple
+drivePower(left_drive,right_drive,False,320)
+dt.turn_until(100,55)
+drivePower(left_drive,right_drive,True, 850)
+dt.turn_until(100,-50)
 drivePower(left_drive,right_drive,True,300)
-dt.turn_until(100,56)
-drivePower(left_drive,right_drive,True,40)
-dt.turn_until(100,-56)
-drivePower(left_drive,right_drive,True,40)
-dt.turn_until(100,-56)
-drivePower(left_drive,right_drive,True,80)
+drivePower(left_drive,right_drive,False,200)
+
 
 #move on to last set of purple
 drivePower(left_drive,right_drive,False,-40)
